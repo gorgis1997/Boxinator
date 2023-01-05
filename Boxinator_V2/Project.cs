5a961411a7c2490e7c40098c548c518df56f14ca
FREEDOM <vinxad@gmail.com>
2023-01-05 19:20:51 +0100

    Initial commit

---
+++ Boxinator_V2/Project.cs
@@ -0,0 +1,12 @@
+﻿namespace Boxinator_V2 {
+    public class Project {
+        private string _name;
+        private string _workPath;
+        
+        public Project(string name, string path) {
+            _name = name;
+            _workPath = path;
+        }
+        
+    }
+}
