8975d516fc8343040b5b728deacfe1f77314715b
FREEDOM <vinxad@gmail.com>
2023-01-12 19:24:58 +0100

    Box dragging, box scaling with window size

--- Boxinator_V2/Usercontrol/dboard.cs
+++ Boxinator_V2/Usercontrol/dboard.cs
@@ -17,8 +17,10 @@ namespace Boxinator_V2.Usercontrol
             InitializeComponent();
         }
         
-        private Project project;
+        private Project _project;
+
         public void dboard_Load(string path, string projectName, bool modeVideo) {
+            Logger.Log("Dashboard loading " + "\n" + "Path: " + path + "\n" + "Project Name: " + projectName + "\n" + "Mode: " + (modeVideo? "Video" : "Image"));
             if (modeVideo) {
                 var converterDialog = new ConverterDialogForm(path);
                 var result = converterDialog.ShowDialog();
@@ -28,23 +30,36 @@ namespace Boxinator_V2.Usercontrol
                 }
                 path = converterDialog.Output();
             }
-            project = new Project(projectName, path);
-            project.InitializeImages();
+            _project = new Project(projectName, path);
+            _project.InitializeImages();
+            
             pictureBox1.Size = panel2.Size;
             pictureBox1.Location = new Point(0, 0);
-            trackBar1.Maximum = project.GetImageCount();
+            
+            trackBar1.Maximum = _project.GetImageCount();
             trackBar1.TickFrequency = trackBar1.Maximum / 10;
             frameLabel.Text = "/ " + trackBar1.Maximum.ToString();
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            this.SizeChanged += new EventHandler(Form1_SizeChanged);
+            Logger.Log("Dashboard loaded");
             SetImage(0);
         }
+        private float _zoom = 1f;
+        private Size _originalSize;
 
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            float zoom = Math.Min((float)pictureBox1.Width / _originalSize.Width, (float)pictureBox1.Height / _originalSize.Height);
+            pictureBox1.ClientSize = new Size((int)(_originalSize.Width * zoom), (int)(_originalSize.Height * zoom));
+        }
         private void SetImage(int index) {
             pictureBox1.Image.Dispose();
-            pictureBox1.Image = project.GetImage(index);
+            pictureBox1.Image = _project.GetImage(index);
+            _originalSize = pictureBox1.Image.Size;
+            _boxes = _project.GetBoxes(index);
+            Logger.Log("Image set to " + index.ToString());
         }
         
-
-
         private void WindowResized(object sender, EventArgs e) {
             pictureBox1.Size = panel2.Size;
             pictureBox1.Location = new Point(0, 0);
@@ -63,6 +78,70 @@ namespace Boxinator_V2.Usercontrol
                     SetImage(value);
                 }
             }
+        }
+        
+        private List<PercentageRectangle> _boxes = new List<PercentageRectangle>();
+        private PercentageRectangle currentBox = new PercentageRectangle(0, 0, 0, 0);
+        private Point startPoint;
+        private bool isDragging = false;
+
+        private void PicturePaint(object sender, PaintEventArgs e) {
+            if (_boxes == null) return;
+            
+            #if DEBUG
+            Logger.Log("Drawing boxes: " + _boxes.Count.ToString() + "\n" + "Current box: " + currentBox.ToString());
+            foreach (var box in _boxes) {
+                Logger.Log(box.ToString());
+            }
+            #endif
+            
+            if (_boxes != null) {
+                // Draw all boxes
+                foreach (var box in _boxes) {
+                    var rectangle = box.GetRectangle(pictureBox1.Size);
+                    e.Graphics.DrawRectangle(Pens.Red, rectangle);
+                }
+            }
+            e.Graphics.DrawRectangle(Pens.Green, currentBox.GetRectangle(pictureBox1.Size));
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e) {
+            if (e.Button != MouseButtons.Left) return;
+            startPoint = e.Location;
+            currentBox = new PercentageRectangle((float)e.X / pictureBox1.Width, (float)e.Y / pictureBox1.Height, 0, 0);
+            isDragging = true;
+            pictureBox1.Invalidate();
+            
+            Logger.LogDebug("Mouse down at " + e.Location.ToString());
+            Logger.LogDebug("Current box: " + currentBox.ToString());
+            Logger.LogDebug("Started dragging");
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e) {
+            if (isDragging) {
+                currentBox.Width = Math.Abs((float)e.X - startPoint.X) / pictureBox1.Width;
+                currentBox.Height = Math.Abs((float)e.Y - startPoint.Y) / pictureBox1.Height;
+                _boxes.Add(currentBox);
+            }
+            isDragging = false;
+            currentBox = new PercentageRectangle(0, 0, 0, 0);
+            Logger.LogDebug("Boxes: " + _boxes.Count.ToString());
+            Logger.Log("Added box " + currentBox.ToString());
+        }
+    
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e) {
+            if (!isDragging) return;
+            currentBox.Width = Math.Abs((float)e.X - startPoint.X) / pictureBox1.Width;
+            currentBox.Height = Math.Abs((float)e.Y - startPoint.Y) / pictureBox1.Height;
+            pictureBox1.Invalidate();
+            Logger.LogDebug("Mouse move at " + e.Location.ToString());
+        }
+
+        private void panel2_SizeChanged(object sender, EventArgs e) {
+            float zoom = Math.Min((float)pictureBox1.Width / _originalSize.Width, (float)pictureBox1.Height / _originalSize.Height);
+            pictureBox1.ClientSize = new Size((int)(_originalSize.Width * zoom), (int)(_originalSize.Height * zoom));
+            pictureBox1.Location = new Point((panel2.Width - pictureBox1.Width) / 2, (panel2.Height - pictureBox1.Height) / 2);
         }
     }
 }
