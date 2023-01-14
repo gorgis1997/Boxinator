1bd4d4056371d7616894f06a9da077f1ff3fb803
FREEDOM <vinxad@gmail.com>
2023-01-14 18:31:05 +0100

    included boxinator default image

--- Boxinator_V2/Properties/Resources.Designer.cs
+++ Boxinator_V2/Properties/Resources.Designer.cs
@@ -18,7 +18,7 @@ namespace Boxinator_V2.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -56,6 +56,16 @@ namespace Boxinator_V2.Properties {
             }
             set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap BOXINATOR_v3 {
+            get {
+                object obj = ResourceManager.GetObject("BOXINATOR_v3", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
             }
         }
         
