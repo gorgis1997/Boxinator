8432a90f9116d2dc4438ac9e8ad1f8d34a8c92c6
FREEDOM <vinxad@gmail.com>
2023-01-16 19:20:59 +0100

    moved forms, added new save form

---
+++ Boxinator_V2/Usercontrol/saveProject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Boxinator_V2.Usercontrol {
+    public partial class saveProject : Form {
+        public saveProject() {
+            InitializeComponent();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e) {
+            DialogResult = DialogResult.Cancel;
+        }
+    }
+}
